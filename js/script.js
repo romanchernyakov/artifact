2aebd12b92c0d6043038e0b8a3f4d255a3535b4f
romanchernyakov <71889266+romanchernyakov@users.noreply.github.com>
2023-02-06 15:22:56 +0300

    v1.01

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import * as ThreeMeshUI from "three-mesh-ui";
+import * as ThreeMeshUI from "../three-mesh-ui";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
